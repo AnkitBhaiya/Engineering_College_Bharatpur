a7a65ac24754c0b22a732f259d942df45c359dcc
Ankit <ankit.sharma722327@gmail.com>
2022-02-16 15:00:44 +0530

    Initial commit

---
+++ app/src/main/java/com/ankitsharma/ecbharatpur/MainActivity.java
@@ -0,0 +1,75 @@
+package com.ankitsharma.ecbharatpur;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+        bottomNavigationView = findViewById (R.id.bottomNavigationView);
+        navController =Navigation.findNavController (this,R.id.frame_layout);
+        drawerLayout = findViewById (R.id.drawerLayout);
+        navigationView = findViewById (R.id.navigation_view);
+        toggle = new ActionBarDrawerToggle (this,drawerLayout,R.string.start,R.string.close);
+        drawerLayout.addDrawerListener (toggle);
+        toggle.syncState ();
+        getSupportActionBar ().setDisplayHomeAsUpEnabled (true);
+        navigationView.setNavigationItemSelectedListener (this);
+
+        NavigationUI.setupWithNavController (bottomNavigationView,navController);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected (item)){
+            return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId ()){
+            case R.id.navigation_developer:
+                Toast.makeText (this, "Developer......", Toast.LENGTH_SHORT).show ();
+            case R.id.navigation_home:
+                Toast.makeText (this, "Home.....", Toast.LENGTH_SHORT).show ();
+            case R.id.navigation_about:
+                Toast.makeText (this, "About Us.....", Toast.LENGTH_SHORT).show ();
+            case R.id.navigation_share:
+                Toast.makeText (this, "Share......", Toast.LENGTH_SHORT).show ();
+            case R.id.navigation_rate:
+                Toast.makeText (this, "Rate Us......", Toast.LENGTH_SHORT).show ();
+            case R.id.navigation_video:
+                Toast.makeText (this, "Video......", Toast.LENGTH_SHORT).show ();
+            case R.id.navigation_ebook:
+                Toast.makeText (this, "Ebook......", Toast.LENGTH_SHORT).show ();
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/ankitsharma/ecbharatpur/ui/home/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.ankitsharma.ecbharatpur.ui.home;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.ankitsharma.ecbharatpur.R;
+
+
+public class HomeFragment extends Fragment {
+    private ImageView map;
+
+        @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+            View view = inflater.inflate (R.layout.fragment_home,container,false);
+
+            map = view.findViewById (R.id.map);
+            map.setOnClickListener (new View.OnClickListener () {
+                @Override
+                public void onClick(View v) {
+                    openMap();
+                }
+
+            });
+
+        return view;
+    }
+
+    private void openMap() {
+            Uri uri = Uri.parse ("geo:0,0?q= Government Engineering College Bharatpur Rajasthan");
+        Intent intent = new Intent (Intent.ACTION_VIEW,uri);
+        intent.setPackage ("com.google.android.apps.maps");
+        startActivity (intent);
+
+    }
+}
